59055120bd00a9dd9155d1f27d38c8e6a1eb3fea
a-quiet-ignition <obatolaraymond@gmail.com>
2025-11-11 16:01:00 +0100

    update relationship_app/views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ def list_book(request):
       """Retrieves all books and renders a template displaying the list."""
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'book_list': books}  # Create a context dictionary with book list
-      return render(request, 'books/list_book.html', context)
+      return render(request, 'relationship_app/list_book.html', context)
 
 
 
@@ -19,4 +19,4 @@ def library_detail(request, library_id):
     """View function to display details of a specific library."""
     library = Library.objects.get(id=library_id)  # Fetch the library instance by ID
     context = {'library': library}  # Create a context dictionary with the library
-    return render(request, 'libraries/library_detail.html', context)
+    return render(request, 'relationship_app/library_detail.html', context)
