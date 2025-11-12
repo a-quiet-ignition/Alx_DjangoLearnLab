b2508f0a9dc033d43fbd988ac8324b3ad2c87207
a-quiet-ignition <obatolaraymond@gmail.com>
2025-11-12 12:00:41 +0100

    add register, login and logout views in relationship_app

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
