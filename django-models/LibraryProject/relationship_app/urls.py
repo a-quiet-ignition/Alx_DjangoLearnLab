018771ee17284c4b2747d75aa6caba101c041aa3
a-quiet-ignition <obatolaraymond@gmail.com>
2025-11-12 11:43:54 +0100

    add register, login and logout views in relationship_app

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
 from .views import list_books
 
 urlpatterns = [
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', views.SignUpView.as_view(), name='register'),
     path('list_books/', views.list_books, name='list_books'),
     path('library_detail/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 
 # Create your views here.
+
+# Registration view
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = '/templates/relationship_app/register.html'
+
+# View to list all books
 def list_books(request):
       """Retrieves all books and renders a template displaying the list."""
       books = Book.objects.all()  # Fetch all book instances from the database
       context = {'list_books': books}  # Create a context dictionary with book list
       return render(request, 'relationship_app/list_books.html', context)
-
-
 
 class LibraryDetailView(DetailView):
     model = Library
