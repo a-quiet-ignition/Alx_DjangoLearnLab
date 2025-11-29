ae150323a4d6d01a62ebb611368948388792af64
a-quiet-ignition <obatolaraymond@gmail.com>
2025-11-29 14:54:30 +0100

    add book model views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
 
 # Create your views here.
+# Generic Views for Book Model
+class BookListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+class BookDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+class BookCreateView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save()
+    
+class BookUpdateView(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
+    def perform_update(self, serializer):
+        serializer.save()
+    
+class BookDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]   
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    
