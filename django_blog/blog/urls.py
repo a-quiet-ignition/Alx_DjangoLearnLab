b9bf2d69381033665e33d33bbe60f1df0e2ef980
a-quiet-ignition <obatolaraymond@gmail.com>
2025-12-06 15:19:26 +0100

    add post views in django_blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import SignUpView, ProfileView, update_profile
+from .views import SignUpView, ProfileView, update_profile, PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 
 urlpatterns = [
@@ -9,4 +9,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('profile/', ProfileView.as_view(), name='home'),
     path('profile/update/', update_profile, name='update_profile'),
+    path('posts/', PostListView.as_view(), name='post_list'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+    path('posts/new/', PostCreateView.as_view(), name='post_create'),
+    path('posts/<int:pk>/edit/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView, DetailView, UpdateView, DeleteView
+from .models import Post
 from .forms import UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -14,7 +16,7 @@ class SignUpView(CreateView):
     
 class ProfileView(TemplateView):
     template_name = 'blog/profile.html'
-
+    
 
 # Profile Management View
 @login_required
@@ -39,3 +41,41 @@ def update_profile(request):
     return render(request, 'blog/update_profile.html', context)
 
 
+# Post Model Views
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['published_date']
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['author', 'title', 'content']
+    success_url = reverse_lazy('post_list')
+    
+class PostUpdateView(UpdateView, UserPassesTestMixin, LoginRequiredMixin):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('post_list')
+    
+    # Permission check for update
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post_list')
+    
+    # Permission check for deletion
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
