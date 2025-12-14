4fccc2b1d83b7b5fb94730551c7cbd974b72543e
a-quiet-ignition <obatolaraymond@gmail.com>
2025-12-14 07:38:16 +0100

    add social_media_api project

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,4 +1,7 @@
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from .models import CustomUser
 
 
@@ -27,9 +30,18 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         user.save()
         return user
     
+# Serializer for token generation
+@receiver(post_save, sender=CustomUser)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+        
     
 # Serializer for updating user profile
 class UserProfileUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['bio', 'profile_picture', 'location']
+        
+        
+get_user_model().objects.create_user
